39144d2bd6d8076b484a60f635cf0d389a530a6f
Swikriti Poudel <poudelswikriti76@gmail.com>
2025-06-23 10:53:11 +0545

    Add functionality to fetch meaning, synonyms, examples, antonynms and other data from dictionary api.

--- script.js
+++ script.js
@@ -20,7 +20,53 @@ dictionaryForm.addEventListener("submit", async event=>{
 
    const meanings = data[0].meanings
 
-   
+   card.innerHTML = ""
+
+   meanings.forEach(meaning=>{
+
+  
+
+    let  storeMeaning = document.createElement("div")
+    storeMeaning.classList.add("storeMeaning")
+    card.appendChild(storeMeaning)
+
+    let partOfSpeech = document.createElement("p")
+    partOfSpeech.textContent = meaning.partOfSpeech
+    partOfSpeech.classList.add("partOfSpeech")
+    storeMeaning.appendChild(partOfSpeech)
+
+
+    meaning.definitions.forEach((defObj,j)=>{
+        let definition = document.createElement("p")
+        definition.textContent = `${j+1}. `+ defObj.definition
+        definition.classList.add("definition")
+        storeMeaning.appendChild(definition)
+
+        if(defObj.example){
+            let example = document.createElement("p")
+             example.textContent = "Example: " +defObj.example
+             example.classList.add("example")
+             storeMeaning.appendChild(example)
+        }
+    })
+
+    
+
+    if(meaning.synonyms.length>0){
+        let synonyms = document.createElement("p")
+        synonyms.textContent = "Synonyms: " +meaning.synonyms.join(", ")
+        synonyms.classList.add("synonyms")
+        storeMeaning.appendChild(synonyms)
+    }
+
+    if(meaning.antonyms.length>0){
+        let antonyms = document.createElement("p")
+        antonyms.textContent = "Antonyms: " + meaning.antonyms.join(", ")
+        antonyms.classList.add("antonyms")
+        storeMeaning.appendChild(antonyms)
+    }
+
+   })
 
    
 
